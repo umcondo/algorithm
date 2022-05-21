af713ac6edb893adcd13c0fa3323cca36c3b06f3
umcondo <superpowerkdg@gmail.com>
2022-05-21 22:58:16 +0900

    직사각형 점찍기

---
+++ 직사각형 점찍기.js
@@ -0,0 +1,12 @@
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+  const n = data.split(" ");
+  const a = Number(n[0]),
+    b = Number(n[1]);
+  // const result = [];
+  for (let i = 0; i < b; i++) {
+    // result.push('*'.repeat(a));
+    console.log("*".repeat(a));
+  }
+  // console.log( result.join('\n'));
+});
